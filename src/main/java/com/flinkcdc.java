f3de731e111fe15c3731464c3c2a6c9b0383ace0
FakeBob <dzbaiyang@hotmail.com>
2022-03-03 17:42:31 +0800

    flink-cdc-update-20220303

--- src/main/java/com/flinkcdc.java
+++ src/main/java/com/flinkcdc.java
@@ -3,8 +3,11 @@ import com.ververica.cdc.connectors.mysql.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.DebeziumSourceFunction;
 import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
-import com.ververica.cdc.debezium.internal.DebeziumChangeConsumer;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackendFactory;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class flinkcdc {
@@ -13,19 +16,28 @@ public class flinkcdc {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
+//        1.1 开启CheckPoint
+
+        env.enableCheckpointing(5000);
+        env.getCheckpointConfig().setCheckpointTimeout(10000);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+
+        env.setStateBackend(new FsStateBackend  ("hdfs://hadoop01:8020/flink-checkpoints"));
+
+
 //        2. 通过FlinkCDC构建SourceFuction
         DebeziumSourceFunction<String> SourceFuction = MySqlSource.<String>builder()
-                .hostname("localhost")
+                .hostname("hadoop01")
                 .port(3306)
                 .username("root")
-                .password("test")
-                .databaseList("test")
-                .tableList()
+                .password("hirisun")
+                .databaseList("fakebob")
+                .tableList("fakebob.test")
                 .deserializer(new StringDebeziumDeserializationSchema())
                 .startupOptions(StartupOptions.initial())
                 .build();
         DataStreamSource<String> dataStreamSource = env.addSource(SourceFuction);
-
 
 //        打印数据
         dataStreamSource.print();
---
+++ src/main/java/com/sinkcdc.java
@@ -0,0 +1,8 @@
+package com;
+import com.flinkcdc.*;
+
+public class sinkcdc {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
