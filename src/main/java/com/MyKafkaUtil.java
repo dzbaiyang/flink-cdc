6388e337c7f039648e2118cd8ac870946e84087e
FakeBob <dzbaiyang@hotmail.com>
2022-03-06 21:02:34 +0800

    FlinkCDC-Simple-2022-03-06

--- src/main/java/com/MyKafkaUtil.java
+++ src/main/java/com/MyKafkaUtil.java
@@ -1,15 +1,42 @@
-package com;
+package com;//package com;
+//
+//import org.apache.flink.api.common.serialization.SimpleStringSchema;
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+//import java.util.Properties;
+//public class MyKafkaUtil {
+//    private static String KAFKA_SERVER = "hadoop01:9092,hadoop02:9092,hadoop03:9092";
+//    private static Properties properties =  new Properties();
+//    static {
+//        properties.setProperty("bootstrap.servers",KAFKA_SERVER);
+//    }
+//    public static FlinkKafkaProducer<String> getKafkaSink(String topic){
+//        return new FlinkKafkaProducer<String>(topic,new SimpleStringSchema(),properties);
+//    }
+//}
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
 import java.util.Properties;
+
 public class MyKafkaUtil {
-    private static String KAFKA_SERVER = "hadoop01:9092,hadoop02:9092,hadoop03:9092";
+    private static String KAFKA_SERVER = "hadoop01:9092,hadoop02:9092,hadoop03:9092";//Kafka的集群和端口
     private static Properties properties =  new Properties();
     static {
         properties.setProperty("bootstrap.servers",KAFKA_SERVER);
     }
+    //返回一个kafka生产者
     public static FlinkKafkaProducer<String> getKafkaSink(String topic){
         return new FlinkKafkaProducer<String>(topic,new SimpleStringSchema(),properties);
+    }
+    // //返回一个kafka消费者
+    public static FlinkKafkaConsumer<String> getKafkaConsumer(String topic,String groupId){
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,KAFKA_SERVER);
+        return new FlinkKafkaConsumer<String>(topic,new SimpleStringSchema(),properties);
+
     }
 }
--- src/main/java/com/flinkcdc_json_kafka.java
+++ src/main/java/com/flinkcdc_json_kafka.java
@@ -1,36 +1,45 @@
 package com;
 
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
 import com.ververica.cdc.connectors.mysql.MySqlSource;
-import com.ververica.cdc.connectors.mysql.table.StartupOptions;
-import com.ververica.cdc.debezium.DebeziumSourceFunction;
-import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
 
 public class flinkcdc_json_kafka {
     public static void main(String[] args) throws Exception {
-//        1. 获取Flink 执行环境
+//1.创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-//        2. 通过FlinkCDC构建SourceFuction
-        DebeziumSourceFunction<String> SourceFuction = MySqlSource.<String>builder()
+        //2.创建Flink-MySQL-CDC的Source
+        DebeziumSourceFunction<String> mysqlSource = MySQLSource.<String>builder()
                 .hostname("hadoop01")
                 .port(3306)
                 .username("root")
                 .password("hirisun")
                 .databaseList("fakebob")
-//                .tableList("fakebob.test")
-//                .deserializer(new JsonDebeziumDeserializationSchema())
-                .startupOptions(StartupOptions.initial())
+//                .tableList("practice.user")
+                .serverTimeZone("Asia/Shanghai")
+//                    .startupOptions(StartupOptions.latest())
+                .startupOptions(StartupOptions.earliest())  //earlist是从binlog第一行开始  但是earlist有个限制就是你必须在建库之前就开启binlog  如果你是中途开启binlog earlist会有问题
+//                    .startupOptions(StartupOptions.initial())
+//                .startupOptions(KafkaOptions.StartupOptions.class)
+                .deserializer(new StringDebeziumDeserializationSchema())//官方告诉需要反序列化
+                .deserializer(new CustomerDeserialization())
                 .build();
-        DataStreamSource<String> dataStreamSource = env.addSource(SourceFuction);
 
-//        3. 打印数据写入Kafka
-        dataStreamSource.print();
-        String SinkTopic = "sync_base_db";
-        dataStreamSource.addSink(MyKafkaUtil.getKafkaSink(SinkTopic));
-        //    启动任务
+        //3.使用CDC Source从MySQL读取数据
+        DataStreamSource<String> mysqlDS = env.addSource(mysqlSource);
+//            DataStreamSource<String> mysqlDS = env.addSource(mysqlSource);
+        mysqlDS.print();
+        //4.打印数据
+
+        mysqlDS.addSink(MyKafkaUtil.getKafkaSink("sync_base_db"));
+
+        //5.执行任务
         env.execute("Flinck-CDC-Kafka");
     }
 }
